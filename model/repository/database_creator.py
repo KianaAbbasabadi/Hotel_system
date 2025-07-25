68be75736a8bc22e64a43dbe9045d45c481906ec
Kiana <abasikian07@gmail.com>
2025-07-25 08:59:16 -0700

    some change

--- model/repository/database_creator.py
+++ model/repository/database_creator.py
@@ -1,1 +1,15 @@
 import sqlite3
+#اتصال
+connection = sqlite3.connect("hotel_db.sqlite")
+#ساخت جدول
+#عملیات ذخیره و ویرایش و حذف و انواع جست و جو و گزارش
+cursor = connection.cursor()
+
+
+
+
+
+
+connection.commit()
+cursor.close()
+connection.close()
