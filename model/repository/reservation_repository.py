2d8288a2d27a4064e67ffcddb20adce2f9ad23ea
Kiana <abasikian07@gmail.com>
2025-07-26 05:49:29 -0700

    database

--- model/repository/reservation_repository.py
+++ model/repository/reservation_repository.py
@@ -1,21 +1,96 @@
+import sqlite3
+from model.entity import reservation
+
 class ReservationRepository:
     def save(self, reservation):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute(
+            """insert into reservations
+                   (reservation_code, guest_name, room_number, check_in_date, nights, payment_status)
+            values (?, ?, ?, ?, ?, ?)""",
+            [reservation.reservation_code, reservation.guest_name, reservation.room_number, reservation.check_in_date, reservation.nights, reservation.payment_status]
+        )
+        connection.commit()
+        cursor.close()
+        connection.close()
+
     def edited(self, reservation):
-        pass
-    def delete(self, code):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("update reservations set guest_name=?, room_number=?, check_in_date=?, nights=?, payment_status=? where reservation_code=?",
+                       [reservation.guest_name, reservation.room_number, reservation.check_in_date, reservation.nights, reservation.payment_status, reservation.reservation_code])
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    def delete(self, reservation_code):
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("delete from reservations where reservation_code= ?", [reservation_code])
+        connection.commit()
+        cursor.close()
+        connection.close()
+
     def find_all(self):
-        pass
-    def find_by_code(self, code):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from reservations")
+        reservations_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return reservations_list
+
+    def find_by_code(self, reservation_code):
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from reservations where reservation_code= ?", [reservation_code])
+        reservation_data = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return reservation_data
+
     def find_by_guest_name(self, guest_name):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from reservations where guest_name= ?", [guest_name])
+        reservations_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return reservations_list
+
     def find_by_check_in_date(self, check_in_date):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from reservations where check_in_date= ?", [check_in_date])
+        reservations_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return reservations_list
+
     def find_by_nights(self, nights):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from reservations where nights= ?", [nights])
+        reservations_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return reservations_list
+
     def find_by_payment_status(self, payment_status):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from reservations where payment_status= ?", [payment_status])
+        reservations_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return reservations_list
+
     def find_by_room_number(self, room_number):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from reservations where room_number= ?", [room_number])
+        reservations_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return reservations_list
--- model/repository/room_repository.py
+++ model/repository/room_repository.py
@@ -1,25 +1,114 @@
+import sqlite3
+from model.entity import room
+
 class RoomRepository:
     def save(self, room):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute(
+            """insert into rooms
+                   (room_code, room_type, price_per_night, floor, is_booked, max_occupancy, has_balcony, view_type)
+            values (?, ?, ?, ?, ?, ?, ?, ?)""",
+            [room.room_code, room.room_type, room.price_per_night, room.floor, room.is_booked, room.max_occupancy, room.has_balcony, room.view_type]
+        )
+        connection.commit()
+        cursor.close()
+        connection.close()
+
     def edited(self, room):
-        pass
-    def delete(self, room):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("update rooms set room_type=?, price_per_night=?, floor=?, is_booked=?, max_occupancy=?, has_balcony=?, view_type=? where room_code=?",
+                       [room.room_type, room.price_per_night, room.floor, room.is_booked, room.max_occupancy, room.has_balcony, room.view_type, room.room_code])
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    def delete(self, room_code):
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("delete from rooms where room_code= ?", [room_code])
+        connection.commit()
+        cursor.close()
+        connection.close()
+
     def find_all(self):
-        pass
-    def find_by_code(self, code):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from rooms")
+        rooms_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return rooms_list
+
+    def find_by_code(self, room_code):
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from rooms where room_code= ?", [room_code])
+        room_data = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return room_data
+
     def find_by_room_type(self, room_type):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from rooms where room_type= ?", [room_type])
+        rooms_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return rooms_list
+
     def find_by_price_per_night(self, price_per_night):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from rooms where price_per_night= ?", [price_per_night])
+        rooms_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return rooms_list
+
     def find_by_floor(self, floor):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from rooms where floor= ?", [floor])
+        rooms_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return rooms_list
+
     def find_by_is_booked(self, is_booked):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from rooms where is_booked= ?", [is_booked])
+        rooms_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return rooms_list
+
     def find_by_max_occupancy(self, max_occupancy):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from rooms where max_occupancy= ?", [max_occupancy])
+        rooms_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return rooms_list
+
     def find_by_has_balcony(self, has_balcony):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from rooms where has_balcony= ?", [has_balcony])
+        rooms_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return rooms_list
+
     def find_by_view_type(self, view_type):
-        pass
+        connection = sqlite3.connect('hotel_db.sqlite')
+        cursor = connection.cursor()
+        cursor.execute("select * from rooms where view_type= ?", [view_type])
+        rooms_list = cursor.fetchall()
+        cursor.close()
+        connection.close()
+        return rooms_list
