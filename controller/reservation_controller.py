0ab1038a489f4e41fe077881a23e10e0f7a3e6c9
Kiana <abasikian07@gmail.com>
2025-07-27 12:04:14 -0700

    reservation controller done

--- controller/reservation_controller.py
+++ controller/reservation_controller.py
@@ -14,35 +14,91 @@ class ReservationController:
             return False, f"Error saving reservation {e}"
 
 
-    def edite(self, reservation):
-        pass
+    def edite(self,  reservation_code, check_in_date , nights, payment_status,room_number, guest_name, total_price, special_requests, is_cancelled=False):
+        try:
+            reservation = Reservation(reservation_code, check_in_date, nights, payment_status, room_number, guest_name,total_price, special_requests, is_cancelled=False)
+            reservation_repository = ReservationRepository()
+            reservation_repository.edite(reservation)
+            return True , f"reservation edited {reservation}"
+        except Exception as e:
+            return False, f"Error editing reservation {e}"
 
     def delete(self, reservation_code):
-        pass
+        try:
+            reservation_repository=ReservationRepository()
+            reservation_repository.delete(reservation_code)
+            return True , f"reservation deleted {reservation_code}"
+        except Exception as e:
+            return False, f"Error deleting reservation {e}"
 
     def find_all(self):
-        pass
+        try:
+            reservation_repository=ReservationRepository()
+            reservations=reservation_repository.find_all()
+            return True, reservations
+        except Exception as e:
+            return False, f"Error finding reservations {e}"
 
     def find_by_code(self, reservation_code):
-        pass
+        try:
+            reservation_repository=ReservationRepository()
+            reservations=reservation_repository.find_by_code(reservation_code)
+            return True, reservations
+        except Exception as e:
+            return False, f"Error finding reservations {e}"
 
     def find_by_guest_name(self, guest_name):
-        pass
+       try:
+           reservation_repository=ReservationRepository()
+           reservations=reservation_repository.find_by_guest_name(guest_name)
+           return True, reservations
+       except Exception as e:
+           return False, f"Error finding reservations  {e}"
 
     def find_by_check_in_date(self, check_in_date):
-        pass
+        try:
+            reservation_repository=ReservationRepository()
+            reservations=reservation_repository.find_by_check_in_date(check_in_date)
+            return True, reservations
+        except Exception as e:
+            return False, f"Error finding reservations check_in_date {e}"
 
     def find_by_nights(self, nights):
-        pass
+        try:
+            reservation_repository=ReservationRepository()
+            reservations=reservation_repository.find_by_nights(nights)
+            return True, reservations
+        except Exception as e:
+            return False, f"Error finding reservations nights {e}"
 
     def find_by_payment_status(self, payment_status):
-        pass
+        try:
+            reservation_repository=ReservationRepository()
+            reservations=reservation_repository.find_by_payment_status(payment_status)
+            return True, reservations
+        except Exception as e:
+            return False, f"Error finding reservations payment_status {e}"
 
     def find_by_room_number(self, room_number):
-        pass
+        try:
+            reservation_repository=ReservationRepository()
+            reservations=reservation_repository.find_by_room_number(room_number)
+            return True, reservations
+        except Exception as e:
+            return False, f"Error finding reservations room_number {e}"
 
     def find_by_total_price(self, total_price):
-        pass
+        try:
+            reservation_repository=ReservationRepository()
+            reservations=reservation_repository.find_by_total_price(total_price)
+            return True, reservations
+        except Exception as e:
+            return False, f"Error finding reservations total price {e}"
 
     def find_by_is_canceled(self, is_canceled):
-        pass
+        try:
+            reservation_repository=ReservationRepository()
+            reservation=reservation_repository.find_by_is_canceled(is_canceled)
+            return True, reservation
+        except Exception as e:
+            return False, f"Error finding reservations {e}"
--- model/repository/reservation_test.py
+++ model/repository/reservation_test.py
@@ -1,5 +1,6 @@
 from model.entity.reservation import Reservation
 from model.repository.reservation_repository import ReservationRepository
+from controller.reservation_controller import ReservationController
 
 #test passed
 reservation_1 = Reservation(
@@ -53,3 +54,29 @@ reservation_repo=ReservationRepository()
 #print(reservation_repo.find_by_total_price(550000))
 #test passed
 #print(reservation_repo.find_by_is_canceled(0))
+
+reservation_controller = ReservationController()
+#test passed
+#print(reservation_controller.save(3 , "2025-01-02" , 3 , "not paid" , 103 , "mobina mousavi" , 60000 , "" ,1))
+#test passed
+#print(reservation_controller.edite(3 , "2025-01-02" , 3 , "not paid" , 103 , "mobina mousavi" , 60000 , "" ,0))
+#test passed
+#print(reservation_controller.delete(2))
+#test passed
+#print(reservation_controller.find_all())
+#test passed
+#print(reservation_controller.find_by_code(3))
+#test passed
+#print(reservation_controller.find_by_guest_name("farnoosh abbasabadi"))
+#test passed
+#print(reservation_controller.find_by_check_in_date("2023-12-15"))
+#test passed
+#print(reservation_controller.find_by_nights(3))
+#test passed
+#print(reservation_controller.find_by_payment_status("Paid"))
+#test passed
+#print(reservation_controller.find_by_room_number(101))
+#test passed
+#print(reservation_controller.find_by_total_price(60000))
+#test passed
+#print(reservation_controller.find_by_is_canceled(0))
