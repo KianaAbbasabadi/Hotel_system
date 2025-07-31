26453a05cc5bd25af1abb55f097a6c85938c1b13
Kiana <abasikian07@gmail.com>
2025-07-31 11:30:18 -0700

    panel view

---
+++ view/panel_view.py
@@ -0,0 +1,35 @@
+from tkinter import *
+import tkinter.ttk as ttk
+from PIL import Image, ImageTk
+from tkinter import messagebox
+
+import tkinter as tk
+from tkinter import PhotoImage
+
+
+window = Tk()
+window.geometry("800x800")
+window.title("Hotel System")
+window.config(cursor="hand2" , background="snow3")
+
+title_lable=Label(window , text="Hotel System" , font=("Arial", 16 , "bold")).place(x=400 , y=50)
+
+
+#img=Image.open("hotel.png")
+#img=img.resize((100,100))
+#img=ImageTk.PhotoImage(img)
+#my_lable=Label(window , image=img)
+#my_lable.place(x=20, y=20)
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
+
